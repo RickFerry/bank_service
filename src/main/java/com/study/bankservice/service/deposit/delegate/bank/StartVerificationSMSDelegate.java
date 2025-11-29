caf2b3b8f3b813088d41da1c4713e14af5485ac8
RickFerry <rick_ferry1@hotmail.com>
2025-11-29 17:36:52 -0300

    Add StartVerificationSMSDelegate for SMS verification process initiation

---
+++ src/main/java/com/study/bankservice/service/deposit/delegate/bank/StartVerificationSMSDelegate.java
@@ -0,0 +1,33 @@
+package com.study.bankservice.service.deposit.delegate.bank;
+
+import com.study.bankservice.model.Client;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component("startVerificationSMSDelegate")
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class StartVerificationSMSDelegate implements JavaDelegate {
+
+    RuntimeService runtimeService;
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        log.info("The startVerificationSMSDelegate has started.");
+
+        String businessKey = delegateExecution.getBusinessKey();
+        Client client = (Client) delegateExecution.getVariable("client");
+
+        runtimeService.createMessageCorrelation("message_start_sms_verification")
+                .processInstanceBusinessKey(businessKey)
+                .setVariable("client", client)
+                .correlateWithResult();
+    }
+}
